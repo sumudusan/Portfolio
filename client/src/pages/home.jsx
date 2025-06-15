57c9a5847352574163d8d117e61704f054509fd5
sumudusan <ss.sumuduaberathne@gmail.com>
2025-06-15 15:16:20 +0400

    add banner image.👍

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,16 @@
+import banner from "../assets/2.jpg"
 export default function Home(){
 
     return(
+        <div className=" w-full min-h-screen text-white">
+            {/*banner image*/}
+          <div className="relative  ">
+            <img 
+             src={banner}
+             alt="banner image"
+             className="w-full object-cover"
+            />
+          </div>
+          </div>
     )
 }
