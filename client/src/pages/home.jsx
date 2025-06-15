629d09fa8aba0756ebe68ea4ac009016463f3a40
sumudusan <ss.sumuduaberathne@gmail.com>
2025-06-15 15:17:49 +0400

    add texts and animations.👍

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,4 +1,7 @@
 import banner from "../assets/2.jpg"
+import { TypeAnimation } from 'react-type-animation';
+
+
 export default function Home(){
 
     return(
@@ -10,6 +13,25 @@ export default function Home(){
              alt="banner image"
              className="w-full object-cover"
             />
+          </div>
+
+          {/*Text*/}
+          <div className="flex-row top-40 absolute pl-10 font-bold">
+            <h1 className="text-3xl py-2">Hello, It's me</h1>
+            <h1 className="text-5xl py-2">Sumudu Sandaruwan</h1>
+             <h1 className="text-3xl py-2 ">I'm a
+                <span className="text-primary-light">
+                <TypeAnimation
+                  sequence={[
+                    " Web Developer", 2000,
+                    " Software Developer", 2000
+                  ]}
+                  wrapper="span"
+                  speed={20}
+                  repeat={Infinity}
+                />
+                </span>
+              </h1>
           </div>
           </div>
     )
