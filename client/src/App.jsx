f4f67e4eea30c2383d34ade86076d4d0d04c4698
sumudusan <ss.sumuduaberathne@gmail.com>
2025-06-24 13:22:48 +0400

    Add routes to App page.✅

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
+import Navbar from "./components/navbar";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import { BrowserRouter, Routes , Route } from 'react-router-dom'
+import Projects from "./pages/projects";
+
 export default function App(){
 
   return(
-    <div>
-      
+    
+    
+     <div>
+      <BrowserRouter>
+      <div
+              className={`fixed top-0 left-0 w-full z-50 transition-transform duration-300 ease-in-out"
+              }`}
+            >
+              <Navbar />
+            </div>
+      <div className="">    
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+      </div>  
+      </BrowserRouter>
     </div>
+    
   )
 }
