01ec7ca15cf76eaedef8a8d4df469950ca0552bb
sumudusan <ss.sumuduaberathne@gmail.com>
2025-06-25 23:59:36 +0400

    Make footer page✅

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+
+    return(
+        <div className=" w-full rounded bg-surface text-white py-3 text-sm text-center">
+            <h1>@2025 Copyright Allright reserved</h1>
+        </div>
+    )
+}
